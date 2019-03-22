bde0a0a16da41b271922bcc8dbc81e144beaa42b
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-03-22 17:53:38 -0300

    Setup configs for limits-service

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
+import com.in28minutes.microservices.limitsservice.config.Configuration;
 import com.in28minutes.microservices.limitsservice.config.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
-        return new LimitConfiguration(1, 1000);
+        return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
     }
 }
